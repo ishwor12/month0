2dd665350b2de585914a7a7a71fff5938861d577
ishwor12 <61532003+ishwor12@users.noreply.github.com>
2026-04-05 16:12:47 +1200

    day5

--- day1/day1/Program.cs
+++ day1/day1/Program.cs
@@ -15,115 +15,25 @@ using System.Xml.Linq;
 
 namespace day1
 {
-    class BankAccount
-    {
-        private string _accountNumber;
-        private double _balance;
-        private string _ownerName;
 
-        public BankAccount(string accountNumber, string ownerName, double initialBalance = 0)
-        {
-            _accountNumber = accountNumber;
-            _ownerName = ownerName;
-            _balance = initialBalance;
-
-        }
-        public void Deposit(double amount)
-        {
-            if (amount <= 0)
-            {
-                Console.WriteLine("Deposit amount must be positive.");
-                return;
-            }
-            _balance += amount;
-            Console.WriteLine("Sucessfully Deposited");
-        }
-        public void Withdraw(double amount)
-        {
-            if (amount <= 0)
-            {
-                Console.WriteLine("Withdrawal amount must be positive.");
-                return;
-            }
-
-            if (amount > _balance)
-            {
-                Console.WriteLine("Insufficient balance.");
-                return;
-            }
-
-            _balance -= amount;
-            Console.WriteLine("Withdraw");
-        }
-        public double GetBalance()
-        {
-            return _balance;
-        }
-        public virtual double CalculateFee()
-        {
-            return 5.00;
-        }
-        public virtual void Display()
-        {
-            Console.WriteLine($"{_ownerName} | Balance: ${_balance:F2}");
-        }
-
-    }
-    class SavingsAccount : BankAccount
-    {
-        public SavingsAccount(string accNo, string Name, double Balance):base(accNo, Name, Balance)
-        {
-
-        }
-        public override double CalculateFee()
-        {
-            return 10.00;
-        }
-
-    }
-        internal class Program
+    internal class Program
     {
 
 
         static void Main(string[] args)
         {
-            BankAccount acc1 = new BankAccount("ACC001", "John", 500);
-            BankAccount acc2 = new BankAccount("ACC002", "Alice", 1000);
-            BankAccount acc3 = new BankAccount("ACC003", "Bob", 200);
-            // Perform transactions
-            acc1.Deposit(200);
-            acc1.Withdraw(100);
-
-            acc2.Withdraw(1500); // should fail
-            acc2.Deposit(300);
-
-            acc3.Withdraw(50);
-            acc3.Deposit(-20); // should fail
-
-            // Print statements
-            acc1.GetBalance();
-            acc2.GetBalance();
-            acc3.GetBalance();
-
-
-
-            List<BankAccount> accounts = new List<BankAccount>
-        {
-            new BankAccount("A001", "John", 500),
-            new SavingsAccount("A002", "Alice", 1500),
-            new SavingsAccount("A003", "Bob", 800),
-             };
-
-foreach (var item in accounts)
+            List<BankAccount> obj = new List<BankAccount>
             {
-                item.Display();
+                new BankAccount("Ishwor", 500),
+                new SavingAccount("Ram", 1500, 0.05m),
+                new CurrentAccount("Hari", 800),
 
-                Console.WriteLine($"Fee: ${item.CalculateFee():F2}");
-                Console.WriteLine("----------------------");
+            };
+            foreach (SavingAccount abc in obj)
+            {
+                Console.WriteLine($"{abc.OwnerName} | Balance: {abc.Balance:C} | Fee: {abc.CalculateFee():C}");
             }
-
-
         }
-    }
 
+    }
 }
