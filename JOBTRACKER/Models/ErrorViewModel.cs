fe0eb37045ece23956f76ae75fb433976efc7cfa
ishwor12 <61532003+ishwor12@users.noreply.github.com>
2026-04-21 20:27:16 +1200

    Initial commit

---
+++ JOBTRACKER/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace JOBTRACKER.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
